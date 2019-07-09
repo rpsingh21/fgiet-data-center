68033210fc788f8f39f347e5d3a903f453be13e5
Rohit Prakash Singh <rohitprakashsingh21@gmail.com>
2019-07-10 00:04:14 +0530

    complete fee submition api

--- api/fees/views.py
+++ api/fees/views.py
@@ -13,14 +13,14 @@ from .serializers import (
 class FeeRegisterAPIView(APIView):
     serializer_class = FeeRegisterFormSerializer
 
-    # def get(self, request, id=None):
-    #     if id:
-    #         instance = FeeRegister.objects.get(id=id)
-    #         serializer = FeeSerializer(instance, context={'request': request})
-    #     else:
-    #         instance = FeeRegister.objects.all()
-    #         serializer = FeeSerializer(instance)
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, id=None):
+        if id:
+            instance = FeeRegister.objects.get(id=id)
+            serializer = FeeSerializer(instance, context={'request': request})
+        else:
+            instance = FeeRegister.objects.all()
+            serializer = FeeSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = FeeRegisterFormSerializer(
