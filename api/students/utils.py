55cbd216f6c3c27bb1afd05219c3c1fb377f7c8a
Rohit Prakash Singh <rohitprakashsingh21@gmail.com>
2019-07-25 23:27:31 +0530

    image compression complete

--- api/students/utils.py
+++ api/students/utils.py
@@ -1,4 +1,8 @@
+from io import BytesIO
 from datetime import datetime
+from PIL import Image
+
+from django.core.files import File
 
 
 def get_session(today=datetime.today()):
@@ -8,3 +12,12 @@ def get_session(today=datetime.today()):
         year -= 1
     session = '{}-{}'.format(year, str(year+1)[-2:])
     return session
+
+
+def compress(image):
+    img = Image.open(image)
+    im_io = BytesIO()
+    img.thumbnail((300, 400))
+    img.save(im_io, 'JPEG', quality=80)
+    new_image = File(im_io, name=image.name)
+    return new_image
